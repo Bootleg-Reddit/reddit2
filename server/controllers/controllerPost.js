8d0b33a88035fd3b0ee688076eaa167e6229f1a2
mohhaidir <khoiruljmbr@gmail.com>
2020-05-20 00:10:30 +0700

    add controller for Post

---
+++ server/controllers/controllerPost.js
@@ -0,0 +1,13 @@
+class ControllerPost {
+  static getPost(req, res, next) {}
+
+  static getPostById(req, res, next) {}
+
+  static createPost(req, res, next) {}
+
+  static deletePost(req, res, next) {}
+
+  static editPost(req, res, next) {}
+}
+
+module.exports = ControllerPost;
