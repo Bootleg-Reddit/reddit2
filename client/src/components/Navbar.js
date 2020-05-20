cf7851f81085cae22c9147d3e3bb56147b9d2f78
Julio Sabandar <kjuliosabandar@gmail.comgit commit test.htmlgit push -u origin mastergit statusgit config --global user.email kjuliosabandar@gmail.com>
2020-05-21 02:35:26 +0700

    implemented modal for login/register

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../assets/logo.svg'
+import LoginForm from './LoginForm'
+import RegisterForm from '../components/RegisterForm'
+
 
 export default function NavBar() {
     const [logIn, setLogIn] = useState(false)
 
+    const [showLogin, setShowLogin] = useState(false);
+    const handleCloseLogin = () => setShowLogin(false);
+    const handleShowLogin = () => setShowLogin(true);
+  
+    const [showRegister, setShowRegister] = useState(false);
+    const handleCloseRegister = () => setShowRegister(false);
+    const handleShowRegister = () => setShowRegister(true);
+
+
     return (
+        <>
             <nav className="navbar row navbar-light bg-light sticky-top">
                     <div className="col-md-2">
                         <Link to="/" className="navbar-brand"><img src={logo} alt="loading" style={{height: "32px", width:"120px"}} /></Link>
@@ -23,10 +36,33 @@ export default function NavBar() {
                         </ul>
                     </div>
                     <div className="col-md-2" style={{float:"inline-end"}}>
-                    <Link to="/login" className="btn btn-outline-primary">LOG IN</Link>
-                    <Link to="/signup" className="btn btn-primary ml-2">SIGN UP</Link>
+                    <div className=" btn btn-outline-primary" onClick={handleShowLogin}>
+                        LOG IN
+                    </div>
+
+                    <div className=" btn btn-primary ml-2" onClick={handleShowRegister}>
+                        SIGN UP
+                    </div>
+
+                    {/* <Link to="/login" className="btn btn-outline-primary">LOG IN</Link>
+                    <Link to="/signup" className="btn btn-primary ml-2">SIGN UP</Link> */}
                     {logIn && <Link to="/" className="btn btn-outline-secondary ml-2">Log Out</Link>}
                     </div>
             </nav>
+            <LoginForm
+            show={showLogin}
+            onHide={handleCloseLogin}
+            onShowRegister={handleShowRegister}
+            animation={true}
+            />
+
+            <RegisterForm
+            show={showRegister}
+            onHide={handleCloseRegister}
+            onShowLogin={handleShowLogin}
+            animation={true}
+            />
+
+        </>
     )
 }
